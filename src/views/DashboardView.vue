26cd57085f3729aad1ada55b7de3f5b9e6af02a7
Andres Diaz <andres@finhubtechnologies.com>
2022-11-22 14:33:06 +0000

    login and dashboard layout

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -5,20 +5,82 @@ import DrawerCrypto from '@/widgets/DrawerCrypto.vue'
 
 const myWallet = computed(getWallet)
 const showDrawerCrypto = ref(false)
+
+const transactionsList = computed(() => {
+  return myWallet.value.transactions.slice(-5).reverse()
+})
 </script>
 
 <template>
-  <button @click="showDrawerCrypto = true">Send crypto</button>
+  <header class="dashboard__header">
+    <img src="/images/logo.png" alt="my-wallet">
+  </header>
 
-  <p>email</p>
-  <div><pre>{{ myWallet.email }}</pre></div>
-  <p>balance</p>
-  <div><pre>{{ myWallet.balance }}</pre></div>
-  <p>transactions</p>
-  <div><pre>{{ myWallet.transactions }}</pre></div>
+  <main class="dashboard__wrapper">
+    <div>
+      <h1 class="dashboard__title">Hi, {{ myWallet.email }}</h1>
+      <div class="dashboard__transactions">
+        <h2 class="dashboard__subtitle">Transactions</h2>
+        <button @click="showDrawerCrypto = true">Send crypto</button>
+      </div>
+      <ul>
+        <li
+          v-for="(transaction, index) in transactionsList"
+          :key="index"
+          class="dashboard__transaction-item"
+        >
+          <div class="dashboard__transaction">
+            <span>{{ transaction.amount }}</span>
+            <span>{{ transaction.currency }}</span>
+            <span>{{ transaction.receiver }}</span>
+            <span>{{ transaction.status }}</span>
+          </div>
+          <p>{{ transaction.description }}</p>
+        </li>
+      </ul>
+    </div>
+  </main>
 
   <DrawerCrypto 
     :is-visible="showDrawerCrypto" 
     @on-close="showDrawerCrypto = false"
   />
 </template>
+
+<style>
+.dashboard__header {
+  border-bottom: 1px solid black;
+}
+
+.dashboard__title {
+  font-size: 24px;
+}
+
+.dashboard__subtitle {
+  font-size: 18px;
+}
+
+.dashboard__wrapper {
+  width: var(--wrapper);
+  margin: 0 auto;
+  padding-top: var(--size-lg);
+}
+
+.dashboard__transactions {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: var(--size-sm);
+}
+
+.dashboard__transaction {
+  display: grid;
+  gap: var(--size-sm);
+  grid-template-columns: repeat(4, 1fr);
+}
+
+.dashboard__transaction-item {
+  border-bottom: 1px solid black;
+  margin-bottom: var(--size-sm);
+}
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,13 +1,15 @@
 <script setup>
 import { reactive } from 'vue'
 import { login } from '@/api/auth'
-import { setLoginData } from '@/models/auth'
+import { getToken, setLoginData, logout } from '@/models/auth'
 import { getRouter } from '@/router'
 
 const formData = reactive({
   username: '',
   password: ''
 })
+
+if (getToken()) logout()
 
 const submit = async () => {
   try {
@@ -21,8 +23,12 @@ const submit = async () => {
 </script>
 
 <template>
-  <main>
-    <form @submit.prevent="submit">
+  <main class="login-view">
+    <img src="/images/logo.png" alt="my-wallet">
+    <form 
+      class="login-view__form"
+      @submit.prevent="submit"
+    >
       <input 
         v-model="formData.username"
         type="text"
@@ -35,3 +41,21 @@ const submit = async () => {
     </form>
   </main>
 </template>
+
+<style>
+.login-view {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+}
+
+.login-view__form {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: var(--width-xs);
+}
+</style>
